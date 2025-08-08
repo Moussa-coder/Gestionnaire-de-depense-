fdb50d094513616fbfc7ba58fca501cbcd0c7112
Moussa <correamoussa469@gmail.com>
2025-08-08 21:42:22 +0000

    Creation du dossier frontend, installation de certain dependences et mise en place du projet

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'backend.urls'
